e42b9cc3f11320e8977966f20967528046650fea
Sodiq-123 <sodiq.agunbiade.4@gmail.com>
2020-04-20 18:04:06 +0100

    Modified the file

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+@app.route('/homepage')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)   
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=5000)
